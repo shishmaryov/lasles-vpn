0c8f6ef79530a920eac54d4d9b836f321881a089
Konstantin <shishmaryovkonstantin@gmail.com>
2021-03-13 23:30:35 +0300

    responsive header

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  $('.header__burger').on('click', function(e) {
+    e.preventDefault;
+    $(this).toggleClass('header__burger--active');
+  });
+})
+
+$(document).ready(function(){
   $('.slider').slick({
     infinite: true,
     dots: true,
@@ -6,4 +13,17 @@ $(document).ready(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
   });
+});
+
+$(document).ready(function(){
+  $('.header__burger').on('click', function(e) {
+    e.preventDefault;
+    $('.header__menu').toggleClass('header__menu--active');
+  });
+});
+
+$(document).ready(function(){
+	$('.header__burger').click(function(){
+		$('.header__menu').toggleClass('header__menu--active');
+	});
 });
